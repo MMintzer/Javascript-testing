31035fe0dc01de4546ba8692d59f3ca68b706459
Matt <mintzer.chemistry@gmail.com>
2018-06-12 09:28:18 -0400

    Reorganize files. Start intro JS problems

---
+++ basic_js_problems/intro_js_problems.js
@@ -0,0 +1,93 @@
+print = console.log(console);
+// madLib
+// Write a function that takes three strings - a verb, an adjective, and a noun - uppercases and interpolates them into the sentence "We shall VERB the ADJECTIVE NOUN". Use ES6 template literals.
+//
+// For example,
+//
+// > madLib('make', 'best', 'guac');
+// "We shall MAKE the BEST GUAC."
+
+function madLib(verb, adjective, noun) {
+  return `We shall ${verb.toUpperCase()} the ${adjective.toUpperCase()} ${noun.toUpperCase()}`
+}
+
+
+
+//using backticks for string to interpolate... INTERESTING!
+
+
+
+// isSubstring
+// Input
+//
+// A String, called searchString.
+// A String, called subString.
+// Output: A Boolean. true if the subString is a part of the searchString.
+//
+// > isSubstring("time to program", "time")
+// true
+//
+// > isSubstring("Jump for joy", "joys")
+// false
+
+function isSubstring(string, substring){
+  return string.includes(substring)
+}
+
+
+
+// Phase II - JS Looping Constructs
+// Carry on! Know your loops!
+//
+// fizzBuzz
+// 3 and 5 are magic numbers.
+
+function fizzBuzz(num){
+  var results = [];
+	for(var i = 1; i <= num; i++){
+		if(i % 5 === 0 && i % 3 === 0){
+			results.push('FizzBuzz');
+		} else if(i % 3 === 0){
+			results.push('Fizz');
+		} else if(i % 5 === 0){
+			results.push('Buzz');
+		} else {
+			results.push(i);
+		}
+	}
+
+  return results;
+}
+
+
+
+
+
+//
+// Define a function fizzBuzz(array) that takes an array and returns a new array of every number in the array that is divisible by either 3 or 5, but not both.
+//
+// isPrime
+// Define a function isPrime(number) that returns true if number is prime. Otherwise, false. Assume number is a positive integer.
+//
+// > isPrime(2)
+// true
+//
+// > isPrime(10)
+// false
+//
+// > isPrime(15485863)
+// true
+//
+// > isPrime(3548563)
+// false
+// sumOfNPrimes
+// Using firstNPrimes, write a function sumOfNPrimes(n) that returns the sum of the first n prime numbers. Hint: use isPrime as a helper method.
+//
+// > sumOfNPrimes(0)
+// 0
+//
+// > sumOfNPrimes(1)
+// 2
+//
+// > sumOfNPrimes(4)
+// 17
