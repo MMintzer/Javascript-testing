d25a2931286f97e568b84d7f8e2c82739ca09ba0
Matt <mintzer.chemistry@gmail.com>
2018-06-12 07:11:13 -0400

    Write bsearch in js

---
+++ bsearch.js
@@ -0,0 +1,23 @@
+// attempting to write bsearch in js and some debugging
+
+var bsearch = function(arr, target){
+  if (arr.length === 0 || (arr.length === 1 && arr[0]!== target)) {
+    return -1;
+  }
+
+  var mid = Math.floor(arr.length / 2); // JS, unlike ruby has every number as a float.  Need to use Math.floor to floor the number
+  if (arr[mid] === target) {
+    return mid;
+  }
+
+  if (target < arr[mid]){
+    return bsearch(arr.slice(0, mid), target);
+  }
+
+  var result = bsearch(arr.slice(mid + 1), target);
+  return (result === -1 ) ? -1 : result + 1 + mid; // Don't forget to add the mid when searching the 'right' half =)
+}
+
+
+
+bsearch([1, 2, 5, 7, 8, 9], 2)
