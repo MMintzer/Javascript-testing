aee635abcabbb3d91a6c68ffba12fda21901fec5
Matt <mintzer.chemistry@gmail.com>
2018-06-14 19:25:28 -0400

    Add myReduce

--- intro_JS_exercises/phase_2_enumerable.js
+++ intro_JS_exercises/phase_2_enumerable.js
@@ -37,3 +37,34 @@ Array.prototype.myMap = function (callback) {
 
   return result;
 }
+
+
+// Array#myReduce(callback[, initialValue]) - (like Ruby's Array#inject) receives a callback function, and optional initial value, returns an accumulator by applying the callback function to each element and the result of the last invocation of the callback (or initial value if supplied)
+// initialValue is optional and should default to the first element of the array if not provided
+// examples:
+// // without initialValue
+// [1, 2, 3].myReduce(function(acc, el) {
+//   return acc + el;
+// }); // => 6
+//
+// // with initialValue
+// [1, 2, 3].myReduce(function(acc, el) {
+//   return acc + el;
+// }, 25); // => 31
+// should also use myEach
+// NB [initialValue] is the conventional way for documentation to express that the args between [ and ] are optional inputs. Your function definition will not include these square brackets.
+
+Array.prototype.myReduce = function (callback, initialValue) {
+  let arr = this;
+
+  if (!initialValue) {
+    initialValue = arr[0];
+    arr = arr.slice(1);
+  }
+
+  arr.myEach(el => initialValue = callback(initialValue, el));
+  return initialValue;
+}
+
+
+console.log([1,2,3].myReduce( (total, item) => total + item ));
