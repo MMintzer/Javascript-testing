1ff92186c1aeb36b61f89428f6459ce92a499cf6
Matt <mintzer.chemistry@gmail.com>
2018-06-15 06:36:31 -0400

    Add recursive range

--- intro_JS_exercises/phase_4_recursion.js
+++ intro_JS_exercises/phase_4_recursion.js
@@ -1,1 +1,11 @@
+//Recursive range
 
+function range(x, y) {
+  if (x === y) {  //base case
+    return [];
+  }
+
+  let call = range(x, y - 1); //inductive step
+  call.push(y - 1);
+  return call
+}
