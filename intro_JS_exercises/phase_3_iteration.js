1ea9d40eb3c55f34782fc5ff710f9f54ce9a5a7c
Matt <mintzer.chemistry@gmail.com>
2018-06-14 20:29:19 -0400

    Add my substrings

--- intro_JS_exercises/phase_3_iteration.js
+++ intro_JS_exercises/phase_3_iteration.js
@@ -18,7 +18,7 @@ Array.prototype.myBubblesort = function() {
 
     for (let i = 0; i < (this.length - 1); i++){
       if (this[i] > this[i + 1]){
-        [this[i], this[i + 1]] = [this[i + 1], this[i]]; // fancy way to get around no parallel assignment
+        [this[i], this[i + 1]] = [this[i + 1], this[i]]; // fancy way to get around no parallel assignment  Wrap it in an array.
         sorted = false
       }
     }
@@ -27,3 +27,15 @@ Array.prototype.myBubblesort = function() {
     return this;
   }
 };
+
+String.prototype.mySubstrings = function(){
+  let substrings = [];
+
+  for (let i = 0; i < this.length; i++) {
+    for (let j = i + 1; j <= this.length; j++) {
+      substrings.push(this.slice(i, j));
+    }
+  }
+
+  return substrings;
+}
