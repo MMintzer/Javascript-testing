3b4d6b5b51c7917a3ca1b964d35751a8007e4936
Matt <mintzer.chemistry@gmail.com>
2018-06-15 06:59:09 -0400

    Add more recursive functions

--- intro_JS_exercises/phase_4_recursion.js
+++ intro_JS_exercises/phase_4_recursion.js
@@ -9,3 +9,35 @@ function range(x, y) {
   call.push(y - 1);
   return call
 }
+
+//Recursive summation
+function sumRec(arr){
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  let lastNum = arr[arr.length - 1];
+  return sumRec(arr.slice(0, arr.length - 1 )) + lastNum
+}
+
+
+//Recursive exponentiation
+function exponentiation(base, exponent) {
+  if (exponent === 0) {
+    return 1;
+  }else {
+    return (base * exponentiation(base, exponent - 1));
+  }
+}
+
+//Recursive fibs
+function fibsRec(n) {
+  if (n < 3) {
+    return [0, 1].slice(0, n);
+  }
+
+  let fibs = fibsRec(n - 1);
+  fibs.push(fibs[fibs.length - 1] + fibs[fibs.length - 2]);
+
+  return fibs;
+}
